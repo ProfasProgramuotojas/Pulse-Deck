9629108ff58680f7ce89c0ed878dd6aefb40dc9c
Mykolas Mickus <mykolas.mickus@gmail.com>
2026-06-06 15:30:53 +0300

    Init

---
+++ src/main/java/com/emcekus/pulsedeck/PulseDeckApplication.java
@@ -0,0 +1,13 @@
+package com.emcekus.pulsedeck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseDeckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PulseDeckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emcekus/pulsedeck/PulseDeckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emcekus.pulsedeck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseDeckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
